339d4f9271600c9876c101c4e1d8cad7edc8c2b6
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-09-08 15:17:17 +0200

    install tawilwind add routing add skeleton components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Views/Home/home.component';
+import { GamesComponent } from './Views/Games/games.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'games/:gameId', component: GamesComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HomeComponent, GamesComponent],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
 })
